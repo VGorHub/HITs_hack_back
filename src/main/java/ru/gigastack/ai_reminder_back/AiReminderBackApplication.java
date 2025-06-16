86c7c7ff52b1ea5f0e88db59c41acffd15f806df
Vladimir Gorohov <vova-gorohov04@mail.ru>
2025-06-16 22:57:26 +0700

    Скелет бэка на Java

---
+++ src/main/java/ru/gigastack/ai_reminder_back/AiReminderBackApplication.java
@@ -0,0 +1,13 @@
+package ru.gigastack.ai_reminder_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiReminderBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiReminderBackApplication.class, args);
+	}
+
+}
